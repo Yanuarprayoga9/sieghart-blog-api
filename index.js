cac78a6f8af0ac6efe9048c9ea426aca7c713f20
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-03-31 14:47:23 +0700

    feat: setup

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const express = require("express");
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send({status:"OK",message:"API SERVICE RUNNING"})
+});
+app.get("/*", (req, res) => {
+    res.send({status:"OK",message:"NOT FOUND"})
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
