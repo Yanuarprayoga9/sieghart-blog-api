286c10e260cf830b095bdfa7bd75e9c27b0dde13
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-04-01 09:24:27 +0700

    fix: error 500

--- index.js
+++ index.js
@@ -40,12 +40,12 @@ app.get('*', (req, res, next) => {
 
 app.listen(PORT, () => console.log('Server ready '));
 
-app.use((err, req, res) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
+// app.use((err, req, res) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || 'Internal Server Error';
+//   res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
