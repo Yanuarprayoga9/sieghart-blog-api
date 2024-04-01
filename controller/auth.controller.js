4c8f20f86f23f3204d5c9fe62efe314ab2cafcd9
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-04-01 08:16:57 +0700

    feat: sign-up feature

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,37 +1,47 @@
 import bcryptjs from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import { errorHandler } from '../middleware/error.js';
+import errorHandler from '../middleware/error.js';
+import User from '../model/user.model.js';
 
 export const signup = async (req, res, next) => {
-  const { username,email, password } = req.body;
+  const { username, email, password } = req.body;
 
   if (
-    !username ||
-    !email ||
-    !password ||
-    username === '' ||
-    email === '' ||
-    password === ''
+    !username
+    || !email
+    || !password
+    || username === ''
+    || email === ''
+    || password === ''
   ) {
     next(errorHandler(400, 'All fields are required'));
   }
+  const existingUser = await User.findOne({ username });
+  if (existingUser) next(errorHandler(400, 'User Already registered'));
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
   try {
-    const validUser = await User.findOne({ email });
-    
+    await newUser.save();
+    res.json('Signup successful');
   } catch (error) {
     next(error);
   }
 };
+
 export const signin = async (req, res, next) => {
-    const { email, password } = req.body;
-  
-    if (!email || !password || email === '' || password === '') {
-      next(errorHandler(400, 'All fields are required'));
-    }
-    try {
-      const validUser = await User.findOne({ email });
-      
-    } catch (error) {
-      next(error);
-    }
-  };
+  const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    console.log(validUser);
+  } catch (error) {
+    next(error);
+  }
+};
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import express from "express";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-import { errorHandler } from "./middleware/error.js";
-import { verifyToken } from "./middleware/verify-user.js";
-import authRoutes from "./routes/auth.route.js";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import errorHandler from './middleware/error.js';
+import { verifyToken } from './middleware/verify-user.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_DB_URL)
   .then(() => {
-    console.log("MongoDb is connected");
+    console.log('MongoDb is connected');
   })
   .catch((err) => {
     console.log(err);
@@ -21,26 +22,26 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-////// ROUTES /////
+/// /// ROUTES /////
 app.use(authRoutes);
-//// ROUTES END ///
+/// / ROUTES END ///
 
-app.get("/", (req, res) => {
-  res.send({ status: "OK", message: "API SERVICE RUNNING" });
+app.get('/', (req, res) => {
+  res.send({ status: 'OK', message: 'API SERVICE RUNNING' });
 });
 
-app.get("/auth-check", verifyToken, (req, res, next) => {
+app.get('/auth-check', verifyToken, (req, res) => {
   res.send(req.user);
 });
-app.get("*", (req, res, next) => {
-  next(errorHandler(404, "NOT FOUND"));
+app.get('*', (req, res, next) => {
+  next(errorHandler(404, 'NOT FOUND'));
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log('Server ready on port 3000.'));
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({
     success: false,
     statusCode,
