52c9d67b354eed274ef41bade5481ef8118505e0
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-04-02 19:46:14 +0700

    test: dummy post json

---
+++ utils/dummy-post.js
@@ -0,0 +1,51 @@
+const postData = [
+  {
+    userId: '660a0d9ae68a714bd38620eb',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: 'Post 1',
+    slug: 'post-1',
+  },
+  {
+    userId: '660a1d20bff9795660ed77fd',
+    content: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    title: 'Post 2',
+    slug: 'post-2',
+  },
+  {
+    userId: '660a0d9ae68a714bd38620eb',
+    content: 'Nulla quis lorem ut libero malesuada feugiat.',
+    title: 'Post 3',
+    slug: 'post-3',
+  },
+  {
+    userId: '660a1d20bff9795660ed77fd',
+    content: 'Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.',
+    title: 'Post 4',
+    slug: 'post-4',
+  },
+  {
+    userId: '660a0d9ae68a714bd38620eb',
+    content: 'Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.',
+    title: 'Post 5',
+    slug: 'post-5',
+  },
+  {
+    userId: '660a1d20bff9795660ed77fd',
+    content: 'Cras ultricies ligula sed magna dictum porta.',
+    title: 'Post 6',
+    slug: 'post-6',
+  },
+  {
+    userId: '660a0d9ae68a714bd38620eb',
+    content: 'Quisque velit nisi, pretium ut lacinia in, elementum id enim.',
+    title: 'Post 7',
+    slug: 'post-7',
+  },
+  {
+    userId: '660a1d20bff9795660ed77fd',
+    content: 'Donec sollicitudin molestie malesuada.',
+    title: 'Post 8',
+    slug: 'post-8',
+  },
+];
+export default postData;
