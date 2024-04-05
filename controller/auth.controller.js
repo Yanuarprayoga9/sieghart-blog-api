a835bae5960e6713fe2c2eabfb43025ea30e34cd
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-04-05 10:17:45 +0700

    feat: google login

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable new-cap */
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import bcryptjs from 'bcryptjs';
-import errorHandler from '../middleware/error.js';
-import User from '../model/user.model.js';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import bcryptjs from "bcryptjs";
+import errorHandler from "../middleware/error.js";
+import User from "../model/user.model.js";
 
 dotenv.config();
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
-    !username
-    || !email
-    || !password
-    || username === ''
-    || email === ''
-    || password === ''
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
   ) {
-    next(errorHandler(400, 'All fields are required'));
+    next(errorHandler(400, "All fields are required"));
   }
   const existingUser = await User.findOne({ username });
-  if (existingUser) next(errorHandler(400, 'User Already registered'));
+  if (existingUser) next(errorHandler(400, "User Already registered"));
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username,
@@ -31,7 +31,7 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.json('Signup successful');
+    res.json("Signup successful");
   } catch (error) {
     next(error);
   }
@@ -40,26 +40,24 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password || email === '' || password === '') {
-    next(errorHandler(400, 'All fields are required'));
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
   }
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(404, 'User Not Found'));
+      return next(errorHandler(404, "User Not Found"));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      return next(errorHandler(400, 'Invalid password'));
+      return next(errorHandler(400, "Invalid password"));
     }
     const token = jwt.sign(
       { id: validUser._id, isAdmin: validUser.isAdmin },
-      process.env.JWT_SECRET,
+      process.env.JWT_SECRET
     );
     const { password: pass, ...rest } = validUser._doc;
-    return res
-      .status(200)
-      .json({'user':rest,token});
+    return res.status(200).json({ user: rest, token });
   } catch (error) {
     return next(error);
   }
@@ -74,3 +72,40 @@ export const me = async (req, res, next) => {
   }
 };
 
+export const google = async (req, res, next) => {
+  const { name, email, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
+      const { password, ...rest } = user._doc;
+      return res.status(200).json({ user: rest, token });
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture: googlePhotoUrl,
+      });
+      await newUser.save();
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
+      const { password, ...rest } = newUser._doc;
+      return res.status(200).json({ user: rest, token });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { me, signin, signout, signup } from '../controller/auth.controller.js';
+import { me, signin, signup } from '../controller/auth.controller.js';
 import { verifyToken } from '../middleware/verify-user.js';
 
 const router = express.Router();
 router.post('/signin', signin);
 router.post('/signup', signup);
 router.get('/me', verifyToken, me);
-router.post('/signout', verifyToken, signout);
 
 export default router;
